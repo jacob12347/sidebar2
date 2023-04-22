1337c5954d10d4ff3a1bca81844bb6b34f623fc2
Chi Pan Yeung <jacabyeung@gmail.com>
2023-04-23 03:55:26 +1000

    first

---
+++ jj.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const fromTop = window.scrollY;
+    const sectionTop = section.offsetTop;
+    if (fromTop >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
